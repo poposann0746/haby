ec2b0d744fb16c5e44233d936fd0dee92f4652d1
poposann0746 <146172257+poposann0746@users.noreply.github.com>
2025-12-08 15:07:25 +0900

    fix:重複Emailによるテストエラーの修正

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
+    user = users(:one)
+
+    sign_in user
     get home_index_url
     assert_response :success
   end
