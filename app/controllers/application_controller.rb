57e803cfdf5bd29e21f1670019f75283fa55772a
poposann0746 <146172257+poposann0746@users.noreply.github.com>
2025-12-08 16:44:07 +0900

    lintチェックの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   # Deviseにnameパラメータを許可する
   def configure_permitted_parameters
     # 新規登録時
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     # アカウント編集時
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 end
