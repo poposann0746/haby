b332073aea4a1daf3d9dd79f1bbb6e13ad93d2e0
poposann0746 <146172257+poposann0746@users.noreply.github.com>
2025-12-09 17:17:42 +0900

    ヘッダーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   get "home/index"
+
+  get "privacy",  to: "static_pages#privacy"
+  get "terms",    to: "static_pages#terms"
+  get "contact",  to: "static_pages#contact"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
