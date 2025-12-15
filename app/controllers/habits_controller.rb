177c16ef78b01769852ea61113aad373be81b962
poposann0746 <146172257+poposann0746@users.noreply.github.com>
2025-12-15 14:12:49 +0900

    習慣編集機能の実装

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,5 +1,6 @@
 class HabitsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_habit, only: %i[show edit update]
 
   def index
     @habits = current_user.habits.order(created_at: :desc)
@@ -25,7 +26,24 @@ class HabitsController < ApplicationController
     @habit = current_user.habits.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @habit.update(habit_params)
+      redirect_to habits_path(@habit), notice: "習慣を更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
+
+  def set_habit
+    @habit = current_user.habits.find(params[:id])
+  end
 
   def habit_params
     params.require(:habit).permit(:name, :detail)
