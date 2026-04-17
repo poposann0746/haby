da3aaad2eb027b2cc64eac8aa1c4f3f2ae6e1a78
poposann0746 <146172257+poposann0746@users.noreply.github.com>
2026-04-17 17:15:04 +0900

    バリデーション強化・修正

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-  validates :name, length: { maximum: 100 }, allow_blank: true
+  validates :name, presence: true, length: { maximum: 100 }, no_html_tags: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :message, presence: true, length: { maximum: 5000 }
+  validates :message, presence: true, length: { maximum: 5000 }, no_html_tags: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_12_071457) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_17_075653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_12_071457) do
     t.datetime "updated_at", null: false
     t.integer "schedule_days", default: [], array: true
     t.index ["schedule_days"], name: "index_habits_on_schedule_days", using: :gin
-    t.index ["user_id", "name"], name: "index_habits_on_user_id_and_name"
+    t.index ["user_id", "name"], name: "index_habits_on_user_id_and_name", unique: true
     t.index ["user_id"], name: "index_habits_on_user_id"
   end
 
